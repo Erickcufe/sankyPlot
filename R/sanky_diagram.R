69c9d5bb5e08a6b0dc059a4cc2dabce87b6fdd67
Erickcufe <erickcufe94@gmail.com>
2023-02-14 14:19:52 -0600

    .

--- R/sanky_diagram.R
+++ R/sanky_diagram.R
@@ -56,3 +56,44 @@ clasif <- vroom::vroom("data/clasificacion281masctl.csv")
 risk <- vroom::vroom("data/risk_stimation_EUR.CSV")
 bin1 <- vroom::vroom("data/Resultados_BIN1-2 (1).csv")
 
+
+remotes::install_github("davidsjoberg/ggsankey")
+library(ggsankey)
+
+
+snps <- vroom::vroom("data/pred_snps.csv")
+snps <- snps[snps$value >= 2.33E-04,]
+snps$value <- NULL
+
+context <- c("3_prime_UTR_variant", "regulatory_region_variant", "synonymous_variant")
+genes <- c("CD36", "BIN1", "BIN1")
+snps$context <- context
+snps$genes <- genes
+snps$BIN1 <- "BIN1"
+
+readr::write_csv(snps, "sanky_data_snps.csv")
+
+cell <- c("")
+
+df <- readr::read_csv("sanky_data_snps.csv")
+df <- df %>%
+  make_long(n1, n2, context, genes, Cell, Expression)
+df
+
+library(ggplot2)
+library(dplyr)
+
+jpeg("sanky_snps.jpeg", units="in", width=10, height=10, res=300)
+ggplot2::ggplot(df, ggplot2::aes(x = x,
+               next_x = next_x,
+               node = node,
+               next_node = next_node,
+               fill = factor(node),
+               label = node)) +
+  geom_sankey(node.color = 1, flow.alpha = 0.7) +
+  geom_sankey_label(color = 1, fill = "white") +
+  theme_sankey(base_size = 16)+
+  ggplot2::theme(legend.position = "none",
+                 text = ggplot2::element_text(size = 25)) +
+  ggplot2::scale_fill_viridis_d(option = "turbo")
+dev.off()
